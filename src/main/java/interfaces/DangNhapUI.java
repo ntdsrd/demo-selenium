d33ca4a381390d1e1a1ad2e7aa97669d4a5120d2
ntdsrd <thedat0309@gmail.com>
2024-01-31 22:51:18 +0700

    feat: add xpath values of Dang Nhap page

---
+++ src/main/java/interfaces/DangNhapUI.java
@@ -0,0 +1,9 @@
+package interfaces;
+
+public class DangNhapUI {
+    public static final String TEN_GIAN_HANG = "//input[@id='Retailer']";
+    public static final String TEN_DANG_NHAP = "//input[@id='UserName']";
+    public static final String MAT_KHAU = "//input[@id='Password']";
+    public static final String BUTTON_QUAN_LY = "//button[@id='btn-login']";
+    public static final String BUTTON_BAN_HANG = "//span[@id='loginNewSale']";
+}
