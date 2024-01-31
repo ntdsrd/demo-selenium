29951f90413a9af70f7f12d38c3773546407a4b5
ntdsrd <thedat0309@gmail.com>
2024-02-01 01:12:00 +0700

    feat: add actions for Tong quan page

---
+++ src/main/java/actions/TongQuan.java
@@ -0,0 +1,17 @@
+package actions;
+
+import actions.commons.BasePage;
+import interfaces.TongQuanUI;
+import org.openqa.selenium.WebDriver;
+
+public class TongQuan extends BasePage {
+    WebDriver driver;
+
+    public TongQuan(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void kiemTraHienThiManHinhTongQuan() {
+        waitForElementVisible(driver, TongQuanUI.TONG_QUAN_HEADER);
+    }
+}
