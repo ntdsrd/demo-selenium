4e6f7e5b2b6d1e68f25aa936cc5ff9ea9eaca9d6
ntdsrd <thedat0309@gmail.com>
2024-01-31 22:06:39 +0700

    feat: add timeout variables

---
+++ src/main/java/actions/commons/GlobalConstants.java
@@ -0,0 +1,7 @@
+package actions.commons;
+
+public class GlobalConstants {
+    public static final String PROJECT_PATH = System.getProperty("user.dir");
+    public static final long SHORT_TIMEOUT = 30;
+    public static final long LONG_TIMEOUT = 50;
+}
