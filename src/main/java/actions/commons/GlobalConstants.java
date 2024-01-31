6cb3ab310042288cb229a2175776493473cfd0ed
ntdsrd <thedat0309@gmail.com>
2024-02-01 00:56:33 +0700

    feat: add data for Dang nhap page

--- src/main/java/actions/commons/GlobalConstants.java
+++ src/main/java/actions/commons/GlobalConstants.java
@@ -2,6 +2,10 @@ package actions.commons;
 
 public class GlobalConstants {
     public static final String PROJECT_PATH = System.getProperty("user.dir");
+    public static final String PROJECT_URL = "https://fnb.kiotviet.vn";
+    public static final String TEN_GIAN_HANG = "automation";
+    public static final String TEN_DANG_NHAP = "0392034354";
+    public static final String MAT_KHAU = "03092000";
     public static final long SHORT_TIMEOUT = 30;
     public static final long LONG_TIMEOUT = 50;
 }
