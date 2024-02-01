192b45d3853d0ace119ee4f99cbed53a013fed04
ntdsrd <thedat0309@gmail.com>
2024-02-02 00:17:43 +0700

    feat: add xpath for Phong/Ban page

---
+++ src/main/java/interfaces/PhongBanUI.java
@@ -0,0 +1,6 @@
+package interfaces;
+
+public class PhongBanUI {
+    public static final String PHONG_BAN_HEADER = "//a[contains(@href,'TableAndRoom')]";
+    public static final String BUTTON_THEM_MOI_PHONG_BAN = "//span[contains(text(),'Thêm phòng/bàn')]//parent::a";
+}
